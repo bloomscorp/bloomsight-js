2b73710337e6bfe3ceda7d3b3bb59bb8acae1bbb
Ayan Kumar Saha <ayan@bloomscorp.com>
2023-09-13 15:50:22 +0530

    feat: add platform detection for opera & opera next

--- src/service/platform.ts
+++ src/service/platform.ts
@@ -28,16 +28,27 @@ export class PlatformService {
 
     private detectBrowser(): void {
 
-        if (this._userAgent.indexOf("Chrome") !== -1) {
+        let nameOffset: number = 0;
+        let verOffset: number = 0;
+
+        if (this._userAgent.indexOf("Opera") !== -1) {
+            this.browser = "Opera";
+        } else if (this._userAgent.indexOf("OPR") !== -1) {
+            this.browser = "Opera Next";
+        } else if (this._userAgent.indexOf("Edge") !== -1) {
+            this.browser = "Microsoft Edge (Legacy)";
+        } else if (this._userAgent.indexOf("Edg") !== -1) {
+            this.browser = "Microsoft Edge (Chromium)";
+        } else if (this._userAgent.indexOf("Chrome") !== -1) {
             this.browser = "Google Chrome";
-        } else if (this._userAgent.indexOf("Firefox") !== -1) {
-            this.browser = "Mozilla Firefox";
         } else if (this._userAgent.indexOf("Safari") !== -1) {
             this.browser = "Apple Safari";
-        } else if (this._userAgent.indexOf("Edge") !== -1) {
-            this.browser = "Microsoft Edge (Chromium-based)";
+        } else if (this._userAgent.indexOf("Firefox") !== -1) {
+            this.browser = "Mozilla Firefox";
         } else if (this._userAgent.indexOf("MSIE") !== -1 || this._userAgent.indexOf("Trident") !== -1) {
-            this.browser = "Internet Explorer";
+            this.browser = "Microsoft Internet Explorer";
+        } else if ((nameOffset = this._userAgent.lastIndexOf(' ') + 1) < (verOffset = this._userAgent.lastIndexOf('/'))) {
+            this.browser = this._userAgent.substring(nameOffset, verOffset);
         } else {
             this.browser = "Unknown";
         }
@@ -48,7 +59,7 @@ export class PlatformService {
         if (this._userAgent.indexOf("Windows") !== -1) {
             this.os = "Windows";
         } else if (this._userAgent.indexOf("Mac OS") !== -1) {
-            this.os = "Mac OS";
+            this.os = "MacOS";
         } else if (this._userAgent.indexOf("Linux") !== -1) {
             this.os = "Linux";
         } else if (this._userAgent.indexOf("Android") !== -1) {
